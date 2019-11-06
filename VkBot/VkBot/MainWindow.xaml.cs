c34f13736eba2f97985cd3f765e8923a18c4dde6
Konstantin2019 <konst_g_p@mail.ru>
2019-11-06 21:47:41 +0300

    Добавлен обработчик события, вызываемый при загрузке окна и запускающий валидацию токена из файла. При неудачной валидации вызывается метод получия uri с токеном. Добавлен обработчик отправки сообщений на кнопку

--- VkBot/VkBot/MainWindow.xaml.cs
+++ VkBot/VkBot/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Windows;
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Windows;
 
 
 namespace VkBot
@@ -9,12 +11,75 @@ namespace VkBot
     public partial class MainWindow : Window
     {
         VkService vkService;
+        ICollection<string> responses;
+        bool valid;
 
         public MainWindow()
         {
             InitializeComponent();
 
             vkService = new VkService();
+            responses = new ObservableCollection<string>();
+
+            Response_view.ItemsSource = responses;
+        }
+
+
+        private async void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            valid = await vkService.ValidateAsync("token.txt");
+
+            if (!valid)
+                await vkService.GetAccessTokenUriAsync();
+        }
+
+        private async void Send_btn_Click(object sender, RoutedEventArgs e)
+        {
+            if (!valid)
+            {
+                var token_uri = token_tb.Text;
+
+                if (token_uri != null && token_uri.Length > 100)
+                {
+                    
+                    var token = await vkService.GetTokenAsync(token_uri);
+                    if (token.Error != null)
+                        MessageBox.Show(token.Error, "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+                    else
+                    {
+                        await vkService.SaveAsync("token.txt", token.Value);
+                        var users = await vkService.GetUsersAsync();
+
+                        if (users.Error != null)
+                            MessageBox.Show(users.Error, "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+                        else
+                        {
+                            var message = Message_box.Text;
+
+                            if (message != null && message.Length > 5)
+                                responses = await vkService.SendAsync(message);
+                            else
+                                MessageBox.Show("Сообщение пустое или слишком короткое!", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+                        }
+                    }
+                }
+            }
+            else
+            {
+                var users = await vkService.GetUsersAsync();
+
+                if (users.Error != null)
+                    MessageBox.Show(users.Error, "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+                else
+                {
+                    var message = Message_box.Text;
+
+                    if (message != null && message.Length > 5)
+                        responses = await vkService.SendAsync(message);
+                    else
+                        MessageBox.Show("Сообщение пустое или слишком короткое!", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+            }
         }
     }
 }
