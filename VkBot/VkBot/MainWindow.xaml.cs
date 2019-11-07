fc51aea897865574a89135fc1d106e330c5d8b16
Konstantin2019 <konst_g_p@mail.ru>
2019-11-07 21:28:37 +0300

    Создан класс ViewModel, реализующий функцию контроллера по взаимодействию vkProdiver с интерфейсом пользователя

--- VkBot/VkBot/MainWindow.xaml.cs
+++ VkBot/VkBot/MainWindow.xaml.cs
@@ -10,62 +10,16 @@ namespace VkBot
     /// </summary>
     public partial class MainWindow : Window
     {
-        VkService vkService;
-        ICollection<string> responses;
-        bool valid;
+        ViewModel viewModel;
 
         public MainWindow()
         {
             InitializeComponent();
 
-            vkService = new VkService();
-            responses = new ObservableCollection<string>();
+            viewModel = new ViewModel();
 
-            Response_view.ItemsSource = responses;
-        }
-
-
-        private async void Window_Loaded(object sender, RoutedEventArgs e)
-        {
-            valid = await vkService.ValidateAsync("token.txt");
-
-            if (!valid)
-                await vkService.GetAccessTokenUriAsync();
-        }
-
-        private async void Send_btn_Click(object sender, RoutedEventArgs e)
-        {
-            if (!valid)
-            {
-                var token_uri = token_tb.Text;
-                var saving_path = "token.txt";
-
-                if (token_uri != null && token_uri.Length > 100)
-                {
-                    
-                    var token = await vkService.GetTokenAsync(token_uri, saving_path);
-                    if (token.Error != null)
-                        MessageBox.Show(token.Error, "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
-                    else
-                    {
-                        var message = Message_box.Text;
-
-                        if (message != null && message.Length > 5)
-                            responses = await vkService.SendAsync(message);
-                        else
-                            MessageBox.Show("Сообщение пустое или слишком короткое!", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
-                    }
-                }
-            }
-            else
-            {
-                var message = Message_box.Text;
-
-                if (message != null && message.Length > 5)
-                    responses = await vkService.SendAsync(message);
-                else
-                    MessageBox.Show("Сообщение пустое или слишком короткое!", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
-            }
+            MainGrid.Loaded += viewModel.ValidateTokenAsync;
+            Send_btn.Click += viewModel.SendMessageAsync;
         }
     }
 }
---
+++ VkBot/VkBot/ViewModel.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace VkBot
+{
+    public class ViewModel
+    {
+        private static VkService vkService = new VkService();
+
+        private bool valid;
+        private string saving_path = "token.txt";
+
+        public string Token_uri { get; set; }
+        public string Message { get; set; }
+
+        public ICollection<string> Responses { get; set; } = new ObservableCollection<string>();
+
+        public async void ValidateTokenAsync(object sender, RoutedEventArgs e)
+        {
+            valid = await vkService.ValidateAsync("token.txt");
+
+            if (!valid)
+                await vkService.GetAccessTokenUriAsync();
+        }
+
+        public async void SendMessageAsync(object sender, RoutedEventArgs e)
+        {
+            if (!valid)
+            {
+                if (Token_uri != null && Token_uri.Length > 100)
+                {
+                    var token = await vkService.GetTokenAsync(Token_uri, saving_path);
+                    if (token.Error != null)
+                        MessageBox.Show(token.Error, "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+                    else
+                        await SendAction();
+                }
+            }
+            else
+                await SendAction();
+        }
+
+        private async Task SendAction()
+        {
+            if (Message != null && Message.Length > 5)
+                Responses = await vkService.SendAsync(Message);
+            else
+                MessageBox.Show("Сообщение пустое или слишком короткое!", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+        }
+    }
+}
