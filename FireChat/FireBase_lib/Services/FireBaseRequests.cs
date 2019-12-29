30c6e99259f2328e6c9dcb1a44705d8c046f9981
Konstantin2019 <konst_g_p@mail.ru>
2019-12-30 00:44:40 +0300

    Попытка устранения бага, при котором закрытие главного окна не позволяет удалить текущего пользователя при последующем закрытии окна чата

--- FireChat/FireBase_lib/Services/FireBaseRequests.cs
+++ FireChat/FireBase_lib/Services/FireBaseRequests.cs
@@ -7,62 +7,69 @@ namespace FireBase_lib.Services
     public class FireBaseRequests
     {
         private const string JSON_SUFFIX = ".json";
-        private HttpClient client;
 
         private string BaseUrl { get; }
 
         public FireBaseRequests(string baseurl)
         {
             BaseUrl = baseurl.EndsWith("/")? baseurl : baseurl + "/";
-            client = new HttpClient();
         }
 
-        private string PathConstruct(string nodepath) => BaseUrl + nodepath.Trim('/') + JSON_SUFFIX;
+        private string FullPath(string nodepath) => BaseUrl + nodepath.Trim('/') + JSON_SUFFIX;
 
         public async Task<bool> Post(string json, string nodepath)
         {
-            try
+            using (var client = new HttpClient())
             {
-                var path = PathConstruct(nodepath);
-                HttpContent content = new StringContent(json);
-                HttpResponseMessage request = await client.PostAsync(path, content);
-                request.EnsureSuccessStatusCode();
-                return true;
-            }
-            catch (Exception)
-            {
-                return false;
+                try
+                {
+                    var path = FullPath(nodepath);
+                    var content = new StringContent(json);
+                    var request = await client.PostAsync(path, content);
+                    request.EnsureSuccessStatusCode();
+                    return true;
+                }
+                catch (Exception)
+                {
+                    return false;
+                }
             }
         }
 
         public async Task<string> Get(string nodepath)
         {
-            try
+            using (var client = new HttpClient())
             {
-                var path = PathConstruct(nodepath);
-                HttpResponseMessage response = await client.GetAsync(path);
-                response.EnsureSuccessStatusCode();
-                string json = await response.Content.ReadAsStringAsync();
-                return json;
-            }
-            catch (Exception)
-            {
-                return null;
+                try
+                {
+                    var path = FullPath(nodepath);
+                    var response = await client.GetAsync(path);
+                    response.EnsureSuccessStatusCode();
+                    var json = await response.Content.ReadAsStringAsync();
+                    return json;
+                }
+                catch (Exception)
+                {
+                    return null;
+                }
             }
         }
 
         public async Task<bool> Delete(string nodepath)
         {
-            try
+            using (var client = new HttpClient())
             {
-                var path = PathConstruct(nodepath);
-                HttpResponseMessage response = await client.DeleteAsync(path);
-                response.EnsureSuccessStatusCode();
-                return true;
-            }
-            catch (Exception)
-            {
-                return false;
+                try
+                {
+                    var path = FullPath(nodepath);
+                    var response = await client.DeleteAsync(path);
+                    response.EnsureSuccessStatusCode();
+                    return true;
+                }
+                catch (Exception)
+                {
+                    return false;
+                }
             }
         }
     }
